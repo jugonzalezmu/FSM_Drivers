77eb4ec8330df167b035e30be9a5222f921dc406
Juan_David_Gonzalez_Muñoz <jugonzalezmu@unal.edu.co>
2024-09-04 19:29:08 -0500

    botones y señales sensores archivo funcionando, se agrego un boton

--- state_UpDown.v
+++ state_UpDown.v
@@ -6,12 +6,12 @@ module state_UpDown(
     input rst,
     input food_button,
     input heal_button,
-    input test_button,
+    input state_button,
+    input test_signal,
 	input light_signal,
 	input echo_signal
 );
     reg test_mode = 0;
-    reg down_mode = 0;
 
     reg upFood = 0;
     reg upSleep= 0;
@@ -29,25 +29,35 @@ Registro_states sleep_register(.state(3'b001), .UpState(upSleep), .clk(clk), .rs
 Registro_states fun_register(.state(3'b010), .UpState(upFun), .clk(clk), .rst(rst));
 Registro_states heal_register(.state(3'b100), .UpState(upHeal), .clk(clk), .rst(rst)); 
 
-    always @(*) begin
+    always @(posedge clk) begin
+        if (rst) begin
+            test_mode <= 0;
+            upFood <= 0;
+            upSleep <= 0;
+            upFun <= 0;
+            upHeal <= 0;
+            selected_state <= 0;
+            up_state <= 0;
+            down_state <= 0;
+        end
+
+        upFood <= 0;
+        upSleep <= 0;
+        upFun <= 0;
+        upHeal <= 0;
+        up_state <= 0;
+        down_state <= 0;
+        test_mode <= (test_signal == 1) ? ~test_mode : test_mode;
+
         if (test_mode == 1) begin
-            test_mode <= (test_button == 1) ? 0 : 1;
-            selected_state <= (food_button == 1) ? ((selected_state == 4) ? 0 : selected_state + 1) : selected_state;
-            //selected_state <= (food_button == 1) ? selected_state + 1 : selected_state;
-            //selected_state <= (selected_state == 4) ? 1 : selected_state;
-            if (down_mode == 0) begin 
-                down_mode <= (heal_button == 1 && state == 5) ? 1 : 0;
-                up_state <= (heal_button == 1 && state < 5) ? 1 : 0;
-            end else begin
-                down_mode <= (heal_button == 1 && state == 1) ? 0 : 1;
-                down_state <= (heal_button == 1 && state > 1) ? 1 : 0;
-            end
+            selected_state <= (state_button == 1) ? ((selected_state == 4) ? 0 : selected_state + 1) : selected_state;
+            up_state <= (food_button == 1) ? 1: 0;
+            down_state <= (heal_button == 1) ? 1: 0;
         end else begin
-            test_mode <= (test_button == 1) ? 1 : 0;
-            upFood <= (food_button == 1) ? 1 : 0;
-            upSleep <= (light_signal == 1) ? 1 : 0;
-            upFun <= (echo_signal == 1) ? 1 : 0;
-            upHeal <= (heal_button == 1) ? 1 : 0;
+            upFood <= food_button;
+            upSleep <= light_signal;
+            upFun <= echo_signal;
+            upHeal <= heal_button;
         end
     end
 
--- state_UpDown_tb.v
+++ state_UpDown_tb.v
@@ -5,94 +5,161 @@ module state_UpDown_tb;
     // Inputs
     reg clk;
     reg rst;
-    reg food_button;
-	reg light_signal;
-	reg echo_signal;
-	reg test_button;
-    reg heal_button;
+    reg food_buttonF;
+	reg light_signalF;
+	reg echo_signalF;
+	reg test_buttonF;
+    reg heal_buttonF;
+	reg state_buttonF;
 
 	// Instantiate the Unit Under Test (UUT)
 
 	state_UpDown uut (
 		.clk(clk),
 		.rst(rst),
-		.food_button(food_button),
-		.light_signal(light_signal),
-		.echo_signal(echo_signal),
-		.test_button(test_button),
-		.heal_button(heal_button)
+		.food_button(food_buttonF),
+		.light_signal(light_signalF),
+		.echo_signal(echo_signalF),
+		.state_button(state_buttonF),
+		.test_signal(test_buttonF),
+		.heal_button(heal_buttonF)
 	);
 
 	initial begin
 		//Initialize Inputs
-		test_button = 0;
 		clk = 0;
 		rst = 0;
-		food_button = 0;
-		light_signal = 0;
-		echo_signal = 0;
-		heal_button = 0;
+		state_buttonF = 0;
+		test_buttonF = 0;
+		food_buttonF = 0;
+		light_signalF = 0;
+		echo_signalF = 0;
+		heal_buttonF = 0;
 		//#10000 es un segundo
-		#100;// Wait 100 ns for global reset to finish
+		#2;// Wait 100 ns for global reset to finish
 		rst = 1;
-		#100;// Wait 100 ns for global reset to finish
+		#2;// Wait 100 ns for global reset to finish
 		rst = 0;
 
-		#100
-		food_button = 1;
-		light_signal = 0;
-		echo_signal = 0;
-		heal_button = 0;
-		#100
-		food_button = 0;
-		light_signal = 1;
-		echo_signal = 0;
-		heal_button = 0;
-		#100
-		food_button = 0;
-		light_signal = 0;
-		echo_signal = 1;
-		heal_button = 0;
-		#100
-		food_button = 0;
-		light_signal = 0;
-		echo_signal = 0;
-		heal_button = 1;
-		#100
-		food_button = 0;
-		light_signal = 0;
-		echo_signal = 0;
-		heal_button = 0;
+		//probando botones test_mode = 0
+		#2
+		food_buttonF = 1;
+		#2
+		food_buttonF = 0;
+		light_signalF = 1;
+		#2
+		light_signalF = 0;
+		echo_signalF = 1;
+		#2
+		echo_signalF = 0;
+		heal_buttonF = 1;
+		#2
+		heal_buttonF = 0;
 
-		#100
-		test_button = 1;
+		#2
+		test_buttonF = 1;
+		#2
+		test_buttonF = 0;
+		//probando botones test_mode = 1
 
-		#100
-		food_button = 1;
-		light_signal = 0;
-		echo_signal = 0;
-		heal_button = 0;
-		#100
-		food_button = 0;
-		light_signal = 1;
-		echo_signal = 0;
-		heal_button = 0;
-		#100
-		food_button = 0;
-		light_signal = 0;
-		echo_signal = 1;
-		heal_button = 0;
-		#100
-		food_button = 0;
-		light_signal = 0;
-		echo_signal = 0;
-		heal_button = 1;
-		#100
-		food_button = 0;
-		light_signal = 0;
-		echo_signal = 0;
-		heal_button = 0;
+		food_buttonF = 0;
+		light_signalF = 0;
+		echo_signalF = 0;
+		heal_buttonF = 0;
 
+		// señales de sensores no dan nada
+		#2
+		light_signalF = 1;
+		#2
+		light_signalF = 0;
+		echo_signalF = 1;
+		#2
+		echo_signalF = 0;
+
+		
+		#2
+		food_buttonF = 1;
+		heal_buttonF = 0;
+		state_buttonF = 0;
+		#2
+		food_buttonF = 1;
+		heal_buttonF = 0;
+		state_buttonF = 0;
+		#2
+		food_buttonF = 0;
+		heal_buttonF = 1;
+		state_buttonF = 0;
+		#2
+		food_buttonF = 0;
+		heal_buttonF = 0;
+		state_buttonF = 1;
+		#2
+		food_buttonF = 0;
+		heal_buttonF = 0;
+		state_buttonF = 1;
+		#2
+		food_buttonF = 0;
+		heal_buttonF = 0;
+		state_buttonF = 1;
+		#2
+		food_buttonF = 0;
+		heal_buttonF = 0;
+		state_buttonF = 1;
+		#2
+		food_buttonF = 0;
+		heal_buttonF = 0;
+		state_buttonF = 1;
+		#2
+		food_buttonF = 0;
+		heal_buttonF = 0;
+		state_buttonF = 1;
+		#2
+		food_buttonF = 0;
+		heal_buttonF = 0;
+		state_buttonF = 1;
+		#2
+		food_buttonF = 0;
+		heal_buttonF = 0;
+		state_buttonF = 1;
+		#2
+		food_buttonF = 0;
+		heal_buttonF = 0;
+		state_buttonF = 1;
+		#2
+		food_buttonF = 0;
+		heal_buttonF = 0;
+		state_buttonF = 1;
+		#2
+		food_buttonF = 0;
+		heal_buttonF = 1;
+		state_buttonF = 0;
+		#2
+		food_buttonF = 1;
+		heal_buttonF = 0;
+		state_buttonF = 0;
+		#2
+		food_buttonF = 0;
+		heal_buttonF = 0;
+		state_buttonF = 0;
+
+
+		#2
+		test_buttonF = 1;
+		#2
+		test_buttonF = 0;
+		#2
+		food_buttonF = 1;
+		#2
+		food_buttonF = 0;
+		light_signalF = 1;
+		#2
+		light_signalF = 0;
+		echo_signalF = 1;
+		#2
+		echo_signalF = 0;
+		heal_buttonF = 1;
+		#2
+		heal_buttonF = 0;
 
 	end
 
@@ -101,8 +168,7 @@ module state_UpDown_tb;
 	initial begin: TEST_CASE
 	    $dumpfile("state_UpDown.vcd");
 		$dumpvars(-1, uut);
-		#10000 $finish;
+		#1000 $finish;
 	end
 	   
 endmodule
-
